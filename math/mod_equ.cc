691f5e8c64a1ac82ad44688e50224b4f7730a744
Abreto Fu <m@abreto.net>
2018-07-30 00:51:22 +0800

    for better reading

--- math/mod_equ.cc
+++ math/mod_equ.cc
@@ -10,11 +10,11 @@ class mod_equ_resolver
     ll a, m;
     inline void gurantee(void)
     {
-        if(a < 0)
+        if ( a < 0 )
         {
-            ll k = (-a) / m;
-            a += (k+1ll) * m;
-            a = (a + m) % m;
+            ll k = ( -a ) / m;
+            a += ( k + 1ll ) * m;
+            a = ( a + m ) % m;
         } else {
             a %= m;
         }
@@ -28,13 +28,13 @@ public:
     }
     ll exgcd(ll m, ll n, ll &x, ll &y)
     {
-        if(0 == n)
+        if ( 0 == n )
         {
             x = 1;
             y = 0;
             return m;
         }
-        ll g = exgcd(n, m%n, x, y);
+        ll g = exgcd( n , m % n , x , y );
         ll t = x;
         x = y;
         y = t - m / n * y;
@@ -43,12 +43,12 @@ public:
     int onemore(ll a2, ll m2)
     {
         ll x, y;
-        ll g = exgcd(m, m2, x, y);
+        ll g = exgcd( m , m2 , x , y );
         assert(x*m+y*m2==g);
         a2 = (a2 + m2) % m2;
-        if (abs(a2-a) % g) return -1;
+        if ( abs( a2 - a ) % g ) return -1;
         ll newm = m / g * m2;
-        ll newa = a + (a2-a) / g * x * m;
+        ll newa = a + ( a2 - a ) / g * x * m;
         a = newa;
         m = newm;
         gurantee();
