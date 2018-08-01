8e017845098835352c7a1fb2a885532022df89f3
Abreto Fu <m@abreto.net>
2018-08-02 02:04:04 +0800

    overload arrow star operator

--- geometry/2d/point.cc
+++ geometry/2d/point.cc
@@ -12,6 +12,7 @@ struct point
     inline point operator-(void) const {return point(-x,-y);}
     inline point operator+(const point& b) const {return point(x+b.x,y+b.y);}
     inline point operator-(const point& b) const {return point(x-b.x,y-b.y);}
+    inline point operator->*(const point &b) const { return (b-(*this)); }
     inline T operator*(const point& b) const {return ((x)*(b.x))+((y)*(b.y));} /* inner product */
     inline T operator^(const point& b) const {return ((x)*(b.y))-((b.x)*(y));} /* outter product */
     inline point& operator+=(const point& b) {point tmp=(*this)+b;(*this)=tmp;return (*this);}
