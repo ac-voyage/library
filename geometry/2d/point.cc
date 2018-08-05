6f086b83bd185f3c0889e4fe17d1f9fa617ef977
Abreto Fu <m@abreto.net>
2018-08-05 20:21:30 +0800

    rewrite point class

--- geometry/2d/point.cc
+++ geometry/2d/point.cc
@@ -1,9 +1,38 @@
 /* 2D Point Class, by Abreto<m@abreto.net> */
 #include <cmath>
 
+/**
+ * Define ABG2d_USE_LL if you want to use long long int for cordnates.
+ */
+
+namespace ab_geometry_2d {
+
 using namespace std;
 
-template <typename T>
+#ifdef ABG2d_USE_LL
+typedef long long int T;
+#else
+typedef double T;
+const T eps = 1e-8;
+#endif
+
+inline T myabs(T x)
+{
+    if(x < 0) return (-x);
+    return x;
+}
+
+inline int sgn(T x)
+{
+    /* no ``difference'' in fact */
+#ifdef ABG2d_USE_LL
+    if (0 == x) return 0;
+#else
+    if (myabs(x) < eps) return 0;
+#endif
+    return (x > 0) ? 1 : -1;
+}
+
 struct point
 {
     T x,y;
@@ -17,8 +46,14 @@ struct point
     inline T operator^(const point& b) const {return ((x)*(b.y))-((b.x)*(y));} /* outter product */
     inline point& operator+=(const point& b) {point tmp=(*this)+b;(*this)=tmp;return (*this);}
     inline point& operator-=(const point& b) {point tmp=(*this)-b;(*this)=tmp;return (*this);}
-    inline bool operator==(const point& b) const {return (x==b.x)&&(y==b.y);}
+    inline bool operator==(const point& b) const {return (0==sgn(x-b.x))&&(0==sgn(y-b.y));}
     inline bool operator!=(const point& b) const {return !((*this)==b);}
 };
 
-#define vec point
+typedef point vec;
+
+#ifdef ABG2d_USE_LL
+#undef ABG2d_USE_LL
+#endif
+
+}   // namespace `ab_geometry_2d`
